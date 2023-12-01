83d56710632de2ffe35148bcc7781e8ecc231eaf
dshuais <1137896420@qq.com>
2023-12-01 15:51:41 +0800

    init

---
+++ app.vue
@@ -0,0 +1,13 @@
+<!--
+ * @Author: dushuai
+ * @Date: 2023-12-01 14:29:58
+ * @LastEditors: dushuai
+ * @LastEditTime: 2023-12-01 15:48:36
+ * @description: 心平气和
+-->
+<template>
+  <div>
+    <!-- <NuxtWelcome /> -->
+    <div>123</div>
+  </div>
+</template>
