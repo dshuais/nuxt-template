f1a8f5671245797e8d5940bb73115c811c344ca5
dshuais <1137896420@qq.com>
2023-12-04 14:35:26 +0800

    feat：新增utils方法

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,13 @@
+/*
+ * @Author: dushuai
+ * @Date: 2023-12-04 09:32:55
+ * @LastEditors: dushuai
+ * @LastEditTime: 2023-12-04 10:54:00
+ * @description: 心平气和
+ */
 /// <reference types="./src/types/app.d.ts" />
+/// <reference types="./src/types/request.d.ts" />
+/// <reference types="./src/types/response.d.ts" />
+/// <reference types="./src/types/global.d.ts" />
 
-export {};
+export { };
